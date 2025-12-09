131b533c205f3a015d6292f0d2dba2e7167b73ed
Divyanshu Prakash <divyanshuprakash914@gmail.com>
2025-12-09 10:23:03 +0530

    updated csv_reader to take input from 2 different file

--- csv_reader.py
+++ csv_reader.py
@@ -1,7 +1,11 @@
-with open("data.csv", "r") as file:
-    data = file.read()
+with open("data.csv", "r") as file1:
+    data = file1.read()
+with open("data1.csv", "r") as file2:
+    data1 = file2.read()
 
-a=data.strip().split("\n")
+b = data.strip() + "\n" + data1.strip()
+a = b.strip().split("\n")
+a = [row for row in a if not row.startswith('"Date"')]
 total_spent = 0
 total_earned = 0
 largest_transaction = ""
@@ -10,7 +14,7 @@ for i in a[1:]:
     b=i.split(",")
     Date=b[0].strip()
     Desc=b[1].strip()
-    Amount=float(b[2].strip())
+    Amount = float(b[2].strip().replace('"', ''))
     if Amount < 0:
         total_spent += abs(Amount)
     else:
@@ -19,6 +23,6 @@ for i in a[1:]:
     if abs(Amount) > abs(largest_transaction) if isinstance(largest_transaction, (int, float)) else True:
         largest_transaction = Amount
         e = Desc
-print(f"Total Spent: ${total_spent:.2f}")
-print(f"Total Earned: ${total_earned:.2f}")
-print(f"Largest Transaction: {e} of ${abs(largest_transaction):.2f}")
+print(f"Total Spent: Rs {total_spent:.2f}")
+print(f"Total Earned: Rs {total_earned:.2f}")
+print(f"Largest Transaction: {e} of Rs {abs(largest_transaction):.2f}")
